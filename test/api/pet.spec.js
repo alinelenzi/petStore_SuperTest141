f2b3ea68f901ea32c2fd3136fd7e0262460e6687
Aline Lenzi <alinelenzi@gmail.com>
2024-08-08 15:28:20 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,17 @@
+const supertest = require('supertest');
+
+const petId = 173218101;
+const lei = 'proibido fumar';
+
+describe('API PetStore Swagger - Entidade Pet', () =>{
+    const request = supertest('https://petstore.swagger.io/v2')
+
+    it('POST Pet', () => {
+        lei
+
+    })
+
+    it('Get Pet', () => {
+
+    })
+})
